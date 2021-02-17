68579f68dac28fb39b21a400153fe04a9474d81d
hyperfraise <damien.menigaux@polytechnique.edu>
2021-02-17 12:39:26 +0100

    create 3d conv ops modules

--- aten/src/ATen/native/metal/MetalPrepackOpRegister.cpp
+++ aten/src/ATen/native/metal/MetalPrepackOpRegister.cpp
@@ -103,6 +103,99 @@ Tensor conv2d_prepack_run(
 #endif
 }
 
+
+c10::intrusive_ptr<Conv3dOpContext> unpack_3d(
+    Tensor&& weight,
+    c10::optional<Tensor>&& bias,
+    std::vector<int64_t>&& stride,
+    std::vector<int64_t>&& padding,
+    std::vector<int64_t>&& dilation,
+    int64_t groups,
+    c10::optional<Scalar> output_min,
+    c10::optional<Scalar> output_max) {
+  const Tensor weightContig = weight.contiguous();
+  const auto ws = weightContig.sizes();
+  auto packed_buffer = permuteWeights(weightContig.data_ptr<float>(), ws.vec());
+  auto packedWeight = at::empty(ws);
+  int64_t size_bytes = at::prod_intlist(ws) * sizeof(float);
+  memcpy(packedWeight.data_ptr(), packed_buffer.data(), size_bytes);
+  return c10::make_intrusive<Conv3dOpContext>(
+      std::move(packedWeight),
+      std::move(bias),
+      stride,
+      padding,
+      dilation,
+      groups,
+      output_min,
+      output_max);
+}
+
+TORCH_LIBRARY_3D(metal, m) {
+  m.class_<Conv3dOpContext>("Conv3dOpContext")
+      .def_pickle(
+          [](const c10::intrusive_ptr<Conv3dOpContext>& op_context)
+              -> SerializationTypeConv3dPrePack { // __getstate__
+            return op_context->pack();
+          },
+          [](SerializationTypeConv3dPrePack state)
+              -> c10::intrusive_ptr<Conv3dOpContext> { // __setstate__
+            return unpack_3d(
+                std::move(std::get<0>(state)),
+                std::move(std::get<1>(state)),
+                std::move(std::get<2>(state)),
+                std::move(std::get<3>(state)),
+                std::move(std::get<4>(state)),
+                std::move(std::get<5>(state)),
+                std::move(std::get<6>(state)),
+                std::move(std::get<7>(state)));
+          });
+  m.def("copy_to_host(Tensor X) -> Tensor Y");
+}
+
+TORCH_LIBRARY_3D(metal_prepack, m) {
+  m.def(
+      "conv3d_prepack(Tensor W, Tensor? B, int[3] stride, "
+      "int[3] padding, int[3] dilation, int groups, "
+      "Scalar? output_min=None, Scalar? output_max=None) "
+      "-> __torch__.torch.classes.metal.Conv3dOpContext");
+  m.def(
+      "conv3d_run(Tensor X, "
+      "__torch__.torch.classes.metal.Conv3dOpContext W_prepack) -> Tensor Y");
+}
+
+c10::intrusive_ptr<Conv3dOpContext> conv3d_prepack(
+    Tensor&& weight,
+    c10::optional<Tensor>&& bias,
+    std::vector<int64_t>&& stride,
+    std::vector<int64_t>&& padding,
+    std::vector<int64_t>&& dilation,
+    int64_t groups,
+    c10::optional<Scalar> output_min,
+    c10::optional<Scalar> output_max) {
+  TORCH_CHECK(weight.dim() == 5);
+  return c10::make_intrusive<Conv3dOpContext>(
+      std::move(weight),
+      std::move(bias),
+      stride,
+      padding,
+      dilation,
+      groups,
+      output_min,
+      output_max);
+}
+
+Tensor conv3d_prepack_run(
+    const Tensor& input,
+    const c10::intrusive_ptr<Conv3dOpContext>& op_context) {
+#if (C10_IOS || TARGET_OS_MAC)
+  return mpscnn::conv3d(input, *op_context);
+#else
+  TORCH_CHECK(false, "conv3d_prepack_run can only be invoked on iOS and MacOS");
+  return input;
+#endif
+}
+
+
 Tensor copy_to_host(const Tensor& input) {
 #if (C10_IOS || TARGET_OS_MAC)
   return mpscnn::copy_to_host(input);
@@ -119,6 +212,17 @@ TORCH_LIBRARY_IMPL(metal_prepack, CPU, m) {
 TORCH_LIBRARY_IMPL(metal_prepack, Metal, m) {
   m.impl("conv2d_run", conv2d_prepack_run);
 }
+
+TORCH_LIBRARY_IMPL(metal_prepack, CPU, m) {
+  m.impl("conv3d_prepack", TORCH_FN(conv3d_prepack));
+}
+
+TORCH_LIBRARY_IMPL(metal_prepack, Metal, m) {
+  m.impl("conv3d_run", conv3d_prepack_run);
+}
+
+
+
 
 TORCH_LIBRARY_IMPL(metal, Metal, m) {
   m.impl("copy_to_host", copy_to_host);
